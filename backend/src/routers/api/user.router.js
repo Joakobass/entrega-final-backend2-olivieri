2a8ed8336108f7b9bbf236d8c1430f9634954404
Joaquin Olivieri <j.olivieri93@gmail.com>
2024-09-09 00:03:40 -0300

    minor changes

--- backend/src/routers/api/user.router.js
+++ backend/src/routers/api/user.router.js
@@ -1,4 +1,4 @@
-import { ADMIN, STANDARD } from "../../constants/roles.constant.js";
+import { STANDARD } from "../../constants/roles.constant.js";
 import UserController from "../../controllers/user.controller.js";
 import BaseRouter from "../base.router.js";
 
@@ -14,76 +14,16 @@ export default class UserRouter extends BaseRouter {
     initialize(){
         const router = this.getRouter();
 
-        this.addGetRoute("/", [], (req, res) => this.#userController.getAll(req, res));
-        this.addGetRoute("/:uid", [], (req, res) => this.#userController.getById(req, res));
-        this.addPostRoute("/", [], (req, res) => this.#userController.create(req, res));
-        this.addPutRoute("/:uid", [], (req, res) => this.#userController.update(req, res));
-        this.addDeleteRoute("/:uid", [], (req, res) => this.#userController.delete(req, res));
+        this.addGetRoute("/", [STANDARD], (req, res) => this.#userController.getAll(req, res));
+        this.addGetRoute("/:uid", [STANDARD], (req, res) => this.#userController.getById(req, res));
+        this.addPostRoute("/", [STANDARD], (req, res) => this.#userController.create(req, res));
+        this.addPutRoute("/:uid", [STANDARD], (req, res) => this.#userController.update(req, res));
+        this.addDeleteRoute("/:uid", [STANDARD], (req, res) => this.#userController.delete(req, res));
 
         // eslint-disable-next-line no-unused-vars
         router.use((error, req, res, next) => {
             res.sendError(error);
         });
     }
-
-    // async #getAll(req, res){
-
-    //     try {
-    //         const users = await this.#UserMgr.getAll();
-    //         res.sendSuccess200(users);
-
-    //     } catch (error) {
-    //         res.sendError(error);
-    //     }
-    // }
-
-    // async #getOneById(req, res){
-
-    //     try {
-    //         const userFound = await this.#UserMgr.getOneById(req.params.uid);
-    //         res.sendSuccess200(userFound);
-
-    //     } catch (error) {
-    //         res.sendError(error);
-    //     }
-    // }
-
-    // async #insert(req, res){
-
-    //     try {
-    //         const newUser = await this.#UserMgr.insertOne(req.body);
-    //         console.log(newUser);
-
-    //         res.sendSuccess201(newUser);
-
-    //     } catch (error) {
-    //         res.sendError(error);
-    //     }
-    // }
-
-    // async #update(req, res){
-
-    //     try {
-    //         const userUpdated = await this.#UserMgr.updateOneById(req.params.uid, req.body);
-
-    //         res.sendSuccess200(userUpdated);
-
-    //     } catch (error) {
-    //         res.sendError(error);
-    //     }
-    // }
-
-    // async #delete(req, res){
-
-    //     try {
-    //         const userDeleted = await this.#UserMgr.deleteOneById(req.params.uid);
-    //         console.log(userDeleted);
-
-    //         res.sendSuccess200(userDeleted);
-
-    //     } catch (error) {
-    //         res.sendError(error);
-    //     }
-    // }
 
 }
